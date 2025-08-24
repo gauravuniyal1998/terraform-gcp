464aefb264e43988ad26253d5967ef6e2e1ce25d
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2025-08-24 18:38:13 +0530

    Update: Added the bucket resource and IAM permissions

--- infra/main.tf
+++ infra/main.tf
@@ -12,3 +12,20 @@ resource "google_storage_bucket" "website" {
   uniform_bucket_level_access = true
   
 }
+
+# Make the bucket publicly accessible
+resource "google_storage_bucket_iam_member" "public_access" {
+  bucket = google_storage_bucket.website.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+#upload index.html to the bucket
+resource "google_storage_bucket_object" "static_site_src" {
+  name   = "index.html"
+  bucket = google_storage_bucket.website.name
+  source = var.static_site_index_path
+  content_type = "text/html" 
+  
+}
+
