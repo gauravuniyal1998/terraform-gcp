a71523b8704eb048e6107166b474fb19216b03a7
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2025-08-25 12:47:11 +0530

    Added: Variable file for the infra bridging GCP and Terraform

---
+++ infra/variable.tf
@@ -0,0 +1,18 @@
+variable "terraform_secret_key" {
+    description = "Secret key for Terraform, provided via environment variable or CI/CD"
+    type        = string
+    sensitive   = true
+
+}
+
+variable "gcp_project_id" {
+    description = "The GCP project ID"
+    type        = string
+
+}
+
+variable "gcp_region" {
+    description = "The GCP region"
+    type        = string
+    default     = "asia-south2"
+}
