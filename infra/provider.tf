02c6eb8b45a7c663de39bf97646534441a81b267
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2025-08-26 14:03:14 +0530

    DELETE: Removed tfstate.lock file- clearing the cache

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,6 @@
 # Configure the Google Cloud provider
-provider "google" {
+/*provider "google" {
     credentials = file("../terraform_key.json")
     project = var.gcp_project_id    
     region  = var.gcp_region
-}
+}*/
